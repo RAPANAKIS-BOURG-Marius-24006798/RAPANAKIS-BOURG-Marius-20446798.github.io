7bc1ad1f9d6a546c373b251dac468a74c5bc1aba
rbmar <marius.rapanakis-bourg@etu.univ-amu.fr>
2025-02-13 13:41:22 +0100

    feat: edit on javascript digital-script.js -WIP-

--- js/digital-script.js
+++ js/digital-script.js
@@ -11,3 +11,20 @@ drawings.forEach((drawing) => {
     drawing.classList.add('active');
 })});
 
+const drawings2 = document.querySelectorAll(".drawing");
+const body = document.querySelector('body');
+drawings2.forEach((drawing) => {
+  drawing.addEventListener('mouseenter', () => {
+    if (drawing.classList.contains('active')) {
+      drawing.classList.add('clear-image');
+      body.classList.add('blur-background');
+    }
+  })
+
+  drawing.addEventListener('mouseleave', () => {
+    if (body.classList.contains('blur-background') || !drawing.classList.contains('active')) {
+      drawing.classList.remove('clear-image');
+      body.classList.remove('blur-background');
+    }
+  })
+});
